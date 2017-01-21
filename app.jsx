ca2a9b4b4a1f50f73897b40efa33e5d3e13a19b3
Subramanian <Subramanian.Palanikumar@td.com>
2017-01-20 22:54:40 -0500

    react seed

--- app.jsx
+++ app.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
 
 class App extends React.Component {
    render() {
       return (
-         <div>
-            Hi Subramanian!!
-         </div>
+       <MuiThemeProvider>
+		<AppBar
+		    title="School App"
+		    iconClassNameRight="muidocs-icon-navigation-expand-more"
+		  />       	
+       </MuiThemeProvider>
+
       );
    }
 }
